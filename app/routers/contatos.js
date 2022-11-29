d9babea469e628f7a023b96fc502cb998caf06b1
cesarhro <cesarhr.olimpio@gmail.com>
2022-11-29 20:29:22 -0300

    fix: Atualizei a rota dos contatos

--- app/routers/contatos.js
+++ app/routers/contatos.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const { contatos } = require("../models/index");
 
-router.get("/", (req, res) => {
-  res.send("Contatos");
+router.get("/", async (req, res) => {
+  const todosContatos = await contatos.findAll();
+  res.json(todosContatos);
+});
+
+router.post("/create", async (req, res) => {
+  const { user_id, tipo, telefone } = req.body;
+
+  await contatos.create({ user_id, tipo, telefone });
+
+  res.send("Abrigo cadastrado com sucesso");
 });
 
 module.exports = router;
