524452a7cedcaac27520e531cba5910752fcb286
cesarhro <cesarhr.olimpio@gmail.com>
2022-11-29 20:30:02 -0300

    update: Adicionei novos packs e modifiquei o app

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 require("dotenv").config();
-const routers = require("./app/routers");
-
 const express = require("express");
+const routers = require("./app/routers");
+const { sequelize } = require("./app/models");
+
 const app = express();
 
-app.listen(process.env.SERVER_PORT, () => {
-  console.log(`Server on in http://localhost:${process.env.SERVER_PORT}`);
-});
-
+app.use(express.json())
 app.use("/", routers);
 
-app.use(express.json);
+sequelize.sync().then(() => {
+  console.log("Db conectado");
+});
+
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(
+    `Servidor online no endereço - http://localhost:${process.env.SERVER_PORT}`
+  );
+});
