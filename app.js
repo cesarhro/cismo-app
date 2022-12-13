4b2e7c683318d5d043566a0f4012ab52a7788d76
cesarhro <cesarhr.olimpio@gmail.com>
2022-12-12 21:31:26 -0300

    Config do vercel

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 require("dotenv").config();
 const express = require("express");
 const routers = require("./api");
-const { sequelize } = require("./models");
 
 const app = express();
 
 app.use(express.json());
 app.use("/", routers);
-
-sequelize.sync().then(() => {
-  console.log("BD conectado");
-});
 
 app.listen(5000, () => {
   console.log(`Servidor online`);
