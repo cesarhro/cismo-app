99502187b7cd6cb1807ad85f4cdfca4deb19a137
cesarhro <cesarhr.olimpio@gmail.com>
2022-12-14 11:54:34 -0300

    Resolvendo erros

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "/.env") });
-
 const cors = require("cors");
 const express = require("express");
 const routes = require("./src/api/index");
