ea057cb792145072a1a057c1ce1d4ab2687b70a8
cesarhro <cesarhr.olimpio@gmail.com>
2022-12-14 23:30:46 -0300

    fix: Replace models name

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -11,7 +11,7 @@ const signup = async (req, res) => {
       senha: await bcrypt.hash(senha, 10),
     };
 
-    const user = await User.create(data);
+    const user = await users.create(data);
 
     if (user) {
       let token = jwt.sign({ id: user.id }, process.env.SECRET_KEY, {
@@ -34,7 +34,7 @@ const login = async (req, res) => {
   try {
     const { email, senha } = req.body;
 
-    const user = await User.findOne({ email });
+    const user = await users.findOne({ email });
 
     if (user) {
       const isSame = await bcrypt.compare(senha, user.senha);
